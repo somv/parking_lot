6888bcf564ac93f58d8d1fb0f950a66c6c951385
Somveer Payal <b0205051@LTB0205051-MAC.local>
2019-04-12 23:50:13 +0530

    git init

---
+++ src/main/java/com/gojek/parkingSolution/App.java
@@ -0,0 +1,13 @@
+package com.gojek.parkingSolution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
